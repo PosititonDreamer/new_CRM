6b9c4433a66b742128a9ef7b8983ba6e35f696c0
PositionDreamer <nulva1@yandex.ru>
2025-10-07 19:43:35 +0500

    crete u-popup

---
+++ src/components/_UIComponents/UPopup/UPopup.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  name: "UPopup",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    }
+  },
+  beforeCreate() {
+    document.body.style.overflow = "hidden";
+  },
+  setup(_, {emit}) {
+
+    const close = () => {
+      document.body.removeAttribute('style');
+      emit("close");
+    }
+
+    return {
+      close
+    }
+  },
+}
+</script>
+
+<template>
+  <Teleport to="body">
+    <div class="u-popup">
+      <div class="u-popup__wrapper">
+        <div class="u-popup__header">
+          <button class="u-popup__close" @click="close"></button>
+          <p class="u-popup__title">{{ title }}</p>
+        </div>
+        <div class="u-popup__content">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<style lang="scss" src="./UPopup.scss" scoped/>
--- src/pages/UI/UI.vue
+++ src/pages/UI/UI.vue
@@ -3,13 +3,15 @@ import UButton from "@/components/_UIComponents/UButton/UButton.vue";
 import UInput from "@/components/_UIComponents/UInput/UInput.vue";
 import {ref} from "vue";
 import UCheckbox from "@/components/_UIComponents/UCheckbox/UCheckbox.vue";
+import UPopup from "@/components/_UIComponents/UPopup/UPopup.vue";
 
 export default {
-  components: {UCheckbox, UInput, UButton},
+  components: {UPopup, UCheckbox, UInput, UButton},
   setup() {
     const text = ref("");
     const area = ref("");
     const checkBoxes = ref([1, 3])
+    const popup = ref(false)
 
     const changeCheckbox = (e) => {
       if(e.checked) {
@@ -20,7 +22,7 @@ export default {
     }
 
     return {
-      text, area, checkBoxes, changeCheckbox
+      text, area, checkBoxes, changeCheckbox, popup
     }
   }
 }
@@ -71,15 +73,68 @@ export default {
       <u-input type="textarea" title="Текстовое поле" v-model="area" error="Ошибка" :start-value="area"/>
     </div>
     <div class="UI__item">
-      <u-checkbox title="Чекбокс 1" name="ui" value="1" :checked="checkBoxes.find(check => check === 1)" @change="changeCheckbox"/>
+      <u-checkbox title="Чекбокс 1" key="ui-check-1" name="ui" value="1" :checked="!!checkBoxes.find(check => check === 1)" @change="changeCheckbox"/>
     </div>
     <div class="UI__item">
-      <u-checkbox title="Чекбокс 2" name="ui" value="2" :checked="checkBoxes.find(check => check === 2)" @change="changeCheckbox"/>
+      <u-checkbox title="Чекбокс 2" key="ui-check-2" name="ui" value="2" :checked="!!checkBoxes.find(check => check === 2)" @change="changeCheckbox"/>
     </div>
     <div class="UI__item">
-      <u-checkbox title="Чекбокс 3" name="ui" value="3" :checked="checkBoxes.find(check => check === 3)" @change="changeCheckbox"/>
+      <u-checkbox title="Чекбокс 3" key="ui-check-3" name="ui" value="3" :checked="!!checkBoxes.find(check => check === 3)" @change="changeCheckbox"/>
     </div>
-
+    <div class="UI__item">
+      <u-button @click="popup = true">Открыть попап</u-button>
+      <u-popup v-if="popup" title="ui попап" @close="popup = false">
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <p>Тут будут тестовые данные</p>
+        <u-button modifier="red">Красная кнопка</u-button>
+        <u-input title="Текстовое поле" v-model="text" :start-value="text"/>
+      </u-popup>
+    </div>
   </div>
 </template>
 
