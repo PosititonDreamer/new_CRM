eae86448df4b99e06150eb7947d3f1658fc21c48
PositionDreamer <nulva1@yandex.ru>
2025-10-11 17:59:40 +0500

    create sidebar

---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,42 @@
+<script>
+import {ref} from "vue";
+
+export default {
+  name: "Sidebar",
+  setup() {
+    const open = ref(false)
+
+    const openSidebar = () => {
+      open.value = true
+      document.body.style.overflow = "hidden"
+    }
+
+    const closeSidebar = () => {
+      open.value = false
+      document.body.removeAttribute("style")
+    }
+
+    return {
+      open,
+      openSidebar,
+      closeSidebar
+    }
+  }
+}
+</script>
+
+<template>
+  <button class="sidebar-open" @click="openSidebar">
+    <span></span>
+    <span></span>
+    <span></span>
+  </button>
+
+  <div :class="['sidebar', {'sidebar--open': open}]">
+    <div class="sidebar__content">
+      <button class="sidebar__close" @click="closeSidebar"></button>
+      Тут будут ссылки
+    </div>
+  </div>
+</template>
+<style lang="scss" src="./Sidebar.scss" scoped />
