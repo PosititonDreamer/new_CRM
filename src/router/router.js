8d87074d2a5ff7b4be2405e69aecf4adab5083b1
PositionDreamer <nulva1@yandex.ru>
2025-10-12 03:53:44 +0500

    add checkUser for router.js

--- src/router/router.js
+++ src/router/router.js
@@ -1,46 +1,72 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import UI from "@/pages/UI/UI.vue";
-import Auth from "@/pages/Auth/Auth.vue";
-import Admin from "@/pages/Admin/Admin.vue";
-import Products from "@/pages/Admin/Products/Products.vue";
+import {createRouter, createWebHistory} from 'vue-router';
+import UIPage from "@/pages/UI/UI.vue";
+import AuthPage from "@/pages/Auth/Auth.vue";
+import AdminPage from "@/pages/Admin/Admin.vue";
+import ProductsPage from "@/pages/Admin/Products/Products.vue";
+import {Auth} from "@/store/workers/Auth.js";
+
 const routes = [
-  {
-    name: 'UI',
-    path: '/ui',
-    component: UI,
-    meta: {
-      layout: 'Sidebar',
+    {
+        name: 'UI',
+        path: '/ui',
+        component: UIPage,
+        meta: {
+            layout: 'Sidebar',
+        },
     },
-  },
-  {
-    name: 'Auth',
-    path: '/',
-    component: Auth,
-    meta: {
-      layout: 'Default',
+    {
+        name: 'Auth',
+        path: '/',
+        component: AuthPage,
+        meta: {
+            layout: 'Default',
+        }
+    },
+    {
+        name: 'Admin',
+        path: '/admin',
+        component: AdminPage,
+        meta: {
+            isAuth: true,
+            isAdmin: true,
+            layout: 'Sidebar',
+        },
+    },
+    {
+        name: "Products",
+        path: '/admin/products',
+        component: ProductsPage,
+        meta: {
+            isAuth: true,
+            isAdmin: true,
+            layout: 'Sidebar',
+        },
     }
-  },
-  {
-    name: 'Admin',
-    path: '/admin',
-    component: Admin,
-    meta: {
-      layout: 'Sidebar',
-    },
-  },
-  {
-    name: "Products",
-    path: '/admin/products',
-    component: Products,
-    meta: {
-      layout: 'Sidebar',
-    },
-  }
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
+
+router.beforeEach(async (to, from, next) => {
+    const token = localStorage.getItem('token')
+    const {checkAuth, getWorker} = Auth();
+
+    if(to.meta.isAuth && to.meta.isAdmin) {
+        if(!getWorker.value.rule && token) {
+            await checkAuth(token)
+        }
+
+        if(getWorker.value.rule && getWorker.value.rule === 'Админ') {
+            next()
+        } else {
+            // todo: Для других ролей
+            next('/')
+        }
+    } else {
+        next()
+    }
+})
 
 export default router;
