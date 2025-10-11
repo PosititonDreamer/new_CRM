290c24b494b7f400154c431fd496a2dcd57f14c4
PositionDreamer <nulva1@yandex.ru>
2025-10-11 20:18:39 +0500

    check auth and change router

--- src/router/router.js
+++ src/router/router.js
@@ -1,14 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import UI from "@/pages/UI/UI.vue";
+import Auth from "@/pages/Auth/Auth.vue";
+import Admin from "@/pages/Admin/Admin.vue";
 const routes = [
   {
     name: 'UI',
-    path: '/',
+    path: '/ui',
     component: UI,
     meta: {
       layout: 'Sidebar',
+    },
+  },
+  {
+    name: 'Auth',
+    path: '/',
+    component: Auth,
+    meta: {
+      layout: 'Default',
     }
   },
+  {
+    name: 'Admin',
+    path: '/admin',
+    component: Admin,
+    meta: {
+      layout: 'Sidebar',
+    }
+  }
 ];
 
 const router = createRouter({
