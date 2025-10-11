49963ebb75cac2fe331116f4d4d5da371dd7f33f
PositionDreamer <nulva1@yandex.ru>
2025-10-11 18:55:26 +0500

    create hook for validate

---
+++ src/hooks/validateInput.js
@@ -0,0 +1,41 @@
+import {computed, ref} from "vue";
+
+export const validateInput = (type, startValue, min = null) => {
+    const data = ref({
+        value: startValue,
+        tacked: false,
+        error: computed(() =>{
+            if(type === "Number") {
+                if(min === null) {
+                    return ""
+                }
+
+                if(data.value.tacked && data.value.value < min) {
+                    return "Обязательное поле для заполнения"
+                }
+
+                return ""
+            }
+
+            if(type === "String") {
+                if(data.value.tacked && data.value.value.trim().length === 0) {
+                    return "Обязательно поле для заполнения"
+                }
+
+                if(data.value.tacked && data.value.value.trim().length < min) {
+                    return `Минимальное количество символов ${min}`
+                }
+
+                return  ""
+            }
+        }),
+        valid: computed(() =>{
+            return data.value.error.trim().length === 0
+        })
+    })
+
+
+    return {
+        data
+    }
+}
