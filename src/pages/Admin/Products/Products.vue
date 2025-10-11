bb27124d7890e434978a1dd89763b2b69468fd34
PositionDreamer <nulva1@yandex.ru>
2025-10-12 02:51:25 +0500

    create products

---
+++ src/pages/Admin/Products/Products.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "Products",
+  components: {},
+  props: {},
+  setup() {}
+}
+</script>
+
+<template>
+  <div class="products">
+    продукты
+  </div>
+</template>
+
+<style lang="scss" src="./Products.scss" scoped />
