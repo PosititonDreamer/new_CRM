9dbdf4f79e4abf25a82d064a8d00f543ac5b948a
PositionDreamer <nulva1@yandex.ru>
2025-10-11 17:59:49 +0500

    create layouts components

---
+++ src/layouts/DefaultLayout/DefaultLayout.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<template>
+  <div class="default-layout">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" src="./DefaultLayout.scss" scoped />
---
+++ src/layouts/SidebarLayout/SidebarLayout.vue
@@ -0,0 +1,18 @@
+<script>
+import Sidebar from "@/components/Sidebar/Sidebar.vue";
+
+export default {
+  name: "SidebarLayout",
+  components: {Sidebar}
+}
+</script>
+<template>
+  <div class="sidebar-layout">
+    <sidebar />
+    <div class="sidebar-layout__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" src="./SidebarLayout.scss" scoped />
