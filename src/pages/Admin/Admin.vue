c3f27cc84dc918db574f70e237c51270499d85f9
PositionDreamer <nulva1@yandex.ru>
2025-10-11 20:17:35 +0500

    create admin-page

---
+++ src/pages/Admin/Admin.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'Admin',
+  setup() {
+
+  }
+}
+</script>
+
+<template>
+  <div class="admin">
+    Админка
+  </div>
+</template>
+<style lang="scss" src="./Admin.scss" scoped />
