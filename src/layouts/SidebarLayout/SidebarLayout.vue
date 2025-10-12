e50eac9d1bac33d7f0ca649725b5240b95176911
PositionDreamer <nulva1@yandex.ru>
2025-10-12 17:32:33 +0500

    update sidebar

--- src/layouts/SidebarLayout/SidebarLayout.vue
+++ src/layouts/SidebarLayout/SidebarLayout.vue
@@ -1,15 +1,24 @@
 <script>
 import Sidebar from "@/components/Sidebar/Sidebar.vue";
+import {useRoute} from "vue-router";
 
 export default {
   name: "SidebarLayout",
-  components: {Sidebar}
+  components: {Sidebar},
+  setup() {
+    const route = useRoute();
+
+    return {
+      route
+    }
+  }
 }
 </script>
 <template>
   <div class="sidebar-layout">
     <sidebar />
     <div class="sidebar-layout__content">
+      <h2 class="sidebar-layout__title">{{route.meta.title}}</h2>
       <slot></slot>
     </div>
   </div>
