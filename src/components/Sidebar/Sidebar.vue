59b1614391ceca0c5140424cc36f2e4bcb7fe8bb
PositionDreamer <nulva1@yandex.ru>
2025-10-13 03:22:52 +0500

    fix sidebar

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -49,10 +49,10 @@ export default {
       <button class="sidebar__close" @click="closeSidebar"></button>
       <div class="sidebar__list">
         <u-accordion class="sidebar__accordion" title="Продукты" :open="openAccordion === 'products'" @open="openAccordion = openAccordion === 'products' ? '' : 'products'">
-          <router-link class="sidebar__link" active-class="sidebar__link--active" to="/admin/products">Продукты</router-link>
-          <router-link class="sidebar__link" active-class="sidebar__link--active" to="/admin/products/other">Кривые продукты</router-link>
-          <router-link class="sidebar__link" active-class="sidebar__link--active" to="/admin/products/measure-units">Единицы измерения</router-link>
-          <router-link class="sidebar__link" active-class="sidebar__link--active" to="/admin/products/packing">Упаковки</router-link>
+          <router-link class="sidebar__link" active-class="sidebar__link--active" :to="{name: 'Products'}">Продукты</router-link>
+          <router-link class="sidebar__link" active-class="sidebar__link--active" :to="{name: 'Others'}">Кривые продукты</router-link>
+          <router-link class="sidebar__link" active-class="sidebar__link--active" :to="{name: 'MeasureUnits'}">Единицы измерения</router-link>
+          <router-link class="sidebar__link" active-class="sidebar__link--active" :to="{name: 'Packing'}">Упаковки</router-link>
         </u-accordion>
       </div>
     </div>
