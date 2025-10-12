5b36a44c16626cd27e89293338ad9c80fbc4a5f7
PositionDreamer <nulva1@yandex.ru>
2025-10-12 17:34:54 +0500

    update sidebar

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,11 +1,14 @@
 <script>
-import {ref} from "vue";
+import {computed, ref} from "vue";
+import UAccordion from "@/components/_UIComponents/UAccordion/UAccordion.vue";
+import {useRoute} from "vue-router";
 
 export default {
   name: "Sidebar",
+  components: {UAccordion},
   setup() {
     const open = ref(false)
-
+    const openAccordion = ref("")
     const openSidebar = () => {
       open.value = true
       document.body.style.overflow = "hidden"
@@ -16,10 +19,19 @@ export default {
       document.body.removeAttribute("style")
     }
 
+
+    const route = useRoute()
+    if(route.meta.page) {
+      openAccordion.value = route.meta.page
+    }
+
+
+
     return {
       open,
       openSidebar,
-      closeSidebar
+      closeSidebar,
+      openAccordion
     }
   }
 }
@@ -35,7 +47,14 @@ export default {
   <div :class="['sidebar', {'sidebar--open': open}]">
     <div class="sidebar__content">
       <button class="sidebar__close" @click="closeSidebar"></button>
-      <router-link class="sidebar__link" active-class="sidebar__link--active" to="/admin/products">Продукты</router-link>
+      <div class="sidebar__list">
+        <u-accordion class="sidebar__accordion" title="Продукты" :open="openAccordion === 'products'" @open="openAccordion = openAccordion === 'products' ? '' : 'products'">
+          <router-link class="sidebar__link" active-class="sidebar__link--active" to="/admin/products">Продукты</router-link>
+          <router-link class="sidebar__link" active-class="sidebar__link--active" to="/admin/products/other">Кривые продукты</router-link>
+          <router-link class="sidebar__link" active-class="sidebar__link--active" to="/admin/products/measure-units">Единицы измерения</router-link>
+          <router-link class="sidebar__link" active-class="sidebar__link--active" to="/admin/products/packing">Упаковки</router-link>
+        </u-accordion>
+      </div>
     </div>
   </div>
 </template>
