48e1dd25baa8e8641e5ebba7b70176ab8a454223
PositionDreamer <nulva1@yandex.ru>
2025-11-24 11:25:27 +0500

    update u-accordion add open ref

--- src/components/_UIComponents/UAccordion/UAccordion.vue
+++ src/components/_UIComponents/UAccordion/UAccordion.vue
@@ -1,13 +1,11 @@
 <script>
+import {ref} from "vue";
+
 export default {
   name: "UAccordion",
   props: {
     title: {
       type: String,
-      required: true,
-    },
-    open: {
-      type: Boolean,
       required: true,
     },
     small: {
@@ -16,14 +14,18 @@ export default {
     }
   },
   setup() {
+    const open = ref(false)
 
+    return {
+      open
+    }
   }
 }
 </script>
 
 <template>
   <div :class="['u-accordion', {'u-accordion--open': open}, {'u-accordion--small': small}]">
-    <p class="u-accordion__header" @click="$emit('open')">
+    <p class="u-accordion__header" @click="open = !open">
       <span>{{title}}</span>
       <svg width="7" height="13" viewBox="0 0 7 13" stroke="currentColor" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M1.32002 11.2L5.52002 6.58001L1.32002 1.96002" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
